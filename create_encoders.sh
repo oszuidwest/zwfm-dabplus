26c586057cfd634a8f4433bb9c19495446774e71
rmens <r.mens@me.com>
2024-03-18 20:22:49 +0100

    Create and push encoder config with GH actions

--- create_encoders.sh
+++ create_encoders.sh
@@ -3,15 +3,15 @@
 # Define the JSON file path
 json_file="/root/stations.json"
 
-# Iterate over each station in the JSON file
-jq -c '.stations[]' "$json_file" | while read -r station; do
+# Use jq to read stations
+jq -c '.stations[]' "$json_file" | while IFS= read -r station; do
   # Extract station details
-  abbreviation=$(echo "$station" | jq -r '.abbreviation')
-  full_name=$(echo "$station" | jq -r '.full_name')
-  livestream=$(echo "$station" | jq -r '.livestream')
-  bitrate=$(echo "$station" | jq -r '.bitrate')
-  port=$(echo "$station" | jq -r '.port')
-
+  abbreviation=$(jq -r '.abbreviation' <<< "$station")
+  full_name=$(jq -r '.full_name' <<< "$station")
+  livestream=$(jq -r '.livestream' <<< "$station")
+  bitrate=$(jq -r '.bitrate' <<< "$station")
+  port=$(jq -r '.port' <<< "$station")
+  
   # Create the directory structure for the station
   mkdir -p "/var/dab/$abbreviation/metadata"
   mkdir -p "/var/dab/$abbreviation/slideshow"
@@ -21,29 +21,5 @@ jq -c '.stations[]' "$json_file" | while read -r station; do
 
   # Create an empty PNG file for the slideshow
   touch "/var/dab/$abbreviation/slideshow/$abbreviation.png"
-
-  # Create the audio configuration file
-  cat << EOF > "/etc/supervisor/conf.d/${abbreviation}-audio.conf"
-[program:dab-${abbreviation}-audio]
-command=odr-audioenc -v ${livestream} -b ${bitrate} -P ${abbreviation}_pad -e tcp://localhost:${port}
-autostart=true
-autorestart=true
-startretries=9999999999999999999999999999999999999999999999999
-stdout_logfile_maxbytes=0MB
-stdout_logfile_backups=0
-stdout_logfile=/var/log/audio_${abbreviation}.log
-EOF
-
-  # Create the metadata configuration file
-  cat << EOF > "/etc/supervisor/conf.d/${abbreviation}-metadata.conf"
-[program:dab-${abbreviation}-metadata]
-command=odr-padenc --dls=/var/dab/$abbreviation/metadata/dls.txt --dir=/var/dab/$abbreviation/slideshow --output=${abbreviation}_pad
-autostart=true
-autorestart=true
-startretries=9999999999999999999999999999999999999999999999999
-stdout_logfile_maxbytes=0MB
-stdout_logfile_backups=0
-stdout_logfile=/var/log/meta_${abbreviation}.log
-EOF
 
 done
